84c06ebc672b1b9a2043bb7677a71316f0d6b850
Onyi Cho <onyicho@gmail.com>
2019-07-02 17:44:57 -0700

    Add user authentiation related controller and model; Adjust route to allow user search, login, logout, and signup

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,41 @@
+class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render 'api/users/show'
+    else
+      render json: { error: @user.error.full_messages }
+    end
+  end
+
+  def update
+    @user = User.find_by_session_token(session[:session_token])
+    if @user
+      @user.update_attributes(user_params)
+      render :show
+    else
+      render json: { error: @user.errors.full_messages }
+    end
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    
+  end
+  
+  def index # Search by username
+    # debugger
+    @users = User.where("username like ?", "%#{params[:username]}%")
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :website, :headline, :profile_img)
+  end
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string(20)       not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  email           :string(100)      not null
+#  headline        :string(140)
+#  website         :string(140)
+#  profile_img     :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+
+
+  validates :username, :email, :session_token, :password_digest, presence: true
+
+  validates :password, length: { minimum: 6, allow_nil: true } 
+
+  before_validation :ensure_session_token
+
+
+  attr_reader :password
+
+
+  def self.find_by_credentials(username, password)
+    @user = User.find_by_username(username)
+    @user && @user.is_password?(password) ? @user : nil
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+  
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+
+end
