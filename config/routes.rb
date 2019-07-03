0132e3d7c15a1f3349f511102a32ccf851e5c239
Onyi Cho <onyicho@gmail.com>
2019-07-02 23:15:09 -0700

    Add root route to bootstrap React components

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  
+  root to: 'static_pages#root' # Route root connection to React frontend
+
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create, :update, :show, :index]
