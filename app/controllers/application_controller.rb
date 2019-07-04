1cca23fc7c3d37b909565503b269f2af0a64b3e7
Onyi Cho <onyicho@gmail.com>
2019-07-03 18:34:42 -0700

    Commit everything in prepare for the next implementation step

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token #Adjust this later
 
   helper_method :current_user, :logged_in?
 
