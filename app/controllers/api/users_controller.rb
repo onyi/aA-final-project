091dbf526db0bfc5375ac1ba743f66b83fd595a3
Onyi Cho <onyicho@gmail.com>
2019-07-04 00:18:47 -0700

    User controller must return error code to represent error for frontend to process correctly

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,7 +6,7 @@ class Api::UsersController < ApplicationController
       login(@user)
       render 'api/users/show'
     else
-      render json: @user.errors.full_messages
+      render json: @user.errors.full_messages, status: 500
     end
   end
 
@@ -16,7 +16,7 @@ class Api::UsersController < ApplicationController
       @user.update_attributes(user_params)
       render :show
     else
-      render json: @user.errors.full_messages
+      render json: @user.errors.full_messages, status: 500
     end
   end
 
