b3cba8b8e45d65df2556a1a3f67334abcf3fb919
Onyi Cho <onyicho@gmail.com>
2019-07-04 20:57:02 -0700

    Add static resources config to package.json and webpack for testing

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,16 @@ module.exports = {
           { loader: 'style-loader' },
           { loader: 'css-loader' }
         ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
       }
     ]
   },
