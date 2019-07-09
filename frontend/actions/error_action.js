88d4d6dcf0242c19821955a03b429810ecd5922d
Onyi Cho <onyicho@gmail.com>
2019-07-09 16:47:55 -0700

    Modify error action to display error properly based on array

--- frontend/actions/error_action.js
+++ frontend/actions/error_action.js
@@ -5,22 +5,44 @@ import React from 'react';
 export const SHOW_ERROR = 'SHOW_ERROR';
 export const REMOVE_ERROR = 'REMOVE_ERROR';
 
-
 export const renderError = (errorMsg, id) => dispatch => {
-  toast(<div className="notification-wrapper">{errorMsg}</div>, {
-    pauseOnHover: false,
-    autoClose: 5000,
-    type: toast.TYPE.ERROR,
-    position: "top-center",
-    onClose: () => {
-      dispatch(removeErrorMessage(id));
+  if (errorMsg.constructor !== Array){
+    errorMsg = [errorMsg]
+  }
+  errorMsg.forEach(
+    error => {
+      toast(<div className="notification-wrapper">{error}</div>, {
+        pauseOnHover: false,
+        autoClose: 5000,
+        type: toast.TYPE.ERROR,
+        position: "top-center",
+        onClose: () => {
+          dispatch(removeErrorMessage(id));
+        }
+      });
+      dispatch({
+        type: SHOW_ERROR,
+        message: error,
+        id
+      })
     }
-  });
-  dispatch({
-    type: SHOW_ERROR,
-    message,
-    id
-  })
+  )
+
+  // TODO: cannot dispatch multiple times...
+  // toast(<div className="notification-wrapper">{errors}</div>, {
+  //   pauseOnHover: false,
+  //   autoClose: 5000,
+  //   type: toast.TYPE.ERROR,
+  //   position: "top-center",
+  //   onClose: () => {
+  //     dispatch(removeErrorMessage(id));
+  //   }
+  // });
+  // dispatch({
+  //   type: SHOW_ERROR,
+  //   message,
+  //   id
+  // })
 }
 
 export const removeErrorMessage = (id) => dispatch => {
