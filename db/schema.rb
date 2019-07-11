a2795a3cc75da80690052f94fcbc76eb4d90d9fe
Onyi Cho <onyicho@gmail.com>
2019-07-10 23:13:33 -0700

    Amazon S3 ActiveStorage configuration related changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_184306) do
+ActiveRecord::Schema.define(version: 2019_07_10_161250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "product_discussions", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "product_id", null: false
+    t.integer "parent_discussion_id"
+    t.string "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_product_discussions_on_author_id"
+    t.index ["parent_discussion_id"], name: "index_product_discussions_on_parent_discussion_id"
+    t.index ["product_id"], name: "index_product_discussions_on_product_id"
+  end
 
   create_table "product_votes", force: :cascade do |t|
     t.integer "product_id", null: false
@@ -49,4 +82,5 @@ ActiveRecord::Schema.define(version: 2019_07_08_184306) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
