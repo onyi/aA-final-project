d155bc7d9a01678c3af7bc95be01dcd01f5a7434
Onyi Cho <onyicho@gmail.com>
2019-07-11 20:28:18 -0700

    Add route to delete post

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create, :update, :show, :index]
-    resources :products, only: [:index, :show, :update, :create] do
+    resources :products, only: [:index, :show, :update, :create, :destroy] do
       resources :discussions, as: :discussions, controller: "product_discussions", only: [:create, :destroy, :index, :show] do
         resources :replies, as: :replies, controller: "product_discussions", only: [:create]
       end
