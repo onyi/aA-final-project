8cb1dac74d13973a57f11c2277cf213e7fa13390
Onyi Cho <onyicho@gmail.com>
2019-08-12 22:20:13 -0700

    Bugfix for discussion upvote cancel

--- app/controllers/api/product_discussion_votes_controller.rb
+++ app/controllers/api/product_discussion_votes_controller.rb
@@ -14,7 +14,7 @@ class Api::ProductDiscussionVotesController < ApplicationController
   end
 
   def destroy
-    @product_discussion_vote = ProductDiscussionVote.find_by_id(params[:id])
+    @product_discussion_vote = ProductDiscussionVote.find_by(discussion_id: params[:id], user_id: current_user.id)
     if @product_discussion_vote
       @product_discussion_vote.destroy
       if !@product_discussion_vote.errors.empty?
